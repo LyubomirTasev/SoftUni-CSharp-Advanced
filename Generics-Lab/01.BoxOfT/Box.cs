37f8f4911a235d690e3f18b16420867c761506c7
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2022-02-07 20:05:19 +0200

    Initial commit

---
+++ Generics-Lab/01.BoxOfT/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.BoxOfT
+{
+    public class Box<T>
+    {
+        private readonly Stack<T> box;
+
+        public Box()
+        {
+            box = new Stack<T>();
+        }
+
+        public int Count => box.Count; // съкратен запис на get return, когато има само един return operator
+
+        public void Add(T element)
+        {
+            box.Push(element);
+        }
+
+        public T Remove() => box.Pop();
+
+        //private List<T> internalList = new List<T>();
+
+        //public int Count
+        //{
+        //    get
+        //    {
+        //        return internalList.Count;
+        //    }
+        //}
+
+        //public void Add(T item)
+        //{
+        //    internalList.Add(item);
+        //}
+
+        //public T Remove()
+        //{
+        //    T element = internalList[internalList.Count - 1];
+        //    internalList.Remove(element);
+        //    //internalList.RemoveAt(internalList.Count - 1);
+        //    return element;
+        //}
+    }
+}
---
+++ Generics-Lab/Generics-Lab/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = CreateArray<int>(10, 1);
+            var stringArr = CreateArray<string>(3, "Sando");
+            var arraysLists = CreateArray<List<string>>(10, new List<string>());
+
+            var list = CreateList<int>(5);
+            var list2 = CreateList<string>("Basi");
+        }
+
+        static T[] CreateArray<T>(int count, T element)
+        {
+            T[] result = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = element;
+            }
+
+            return result;
+        }
+
+        static List<T> CreateList<T>(T item)
+        {
+            List<T> list = new List<T>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(item);
+            }
+
+            return list;
+        }
+
+        //static string[] CreateArrayOfString(int count, string element)
+        //{
+        //    string[] result = new string[count];
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        result[i] = element;
+        //    }
+
+        //    return result;
+        //}
+    }
+}
